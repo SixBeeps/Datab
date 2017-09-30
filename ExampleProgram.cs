e47bb57ea8b5a48b31993a20cd6dd353f1cdc764
Brandon Lee <brandononlyu@gmail.com>
2017-09-30 12:35:44 -0400

    Made a better ExampleProgram.cs

--- ExampleProgram.cs
+++ ExampleProgram.cs
@@ -1,26 +1,39 @@
 using System;
 using System.Collections.Generic;
-using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Datab;
 
-namespace Code {
-  public static class DatabExample {
-    public static Datab.Database database;
-    public static void Main(String[] args) {
-      database.Init();
-      foreach (List<string> secondaryList in database.GetAllContent()) {
-        foreach (string s in secondaryList) {
-          Console.WriteLine(s);
+namespace DatabTesting
+{
+    class Program
+    {
+        public static Database database = new Database();
+        public static void Main(string[] args)
+        {
+            database.Init();
+            while(database == null)
+            {
+                GC.Collect();
+            }
+            database.SetLabel(5, "Username");
+            database.SetLabel(6, "Password");
+            OutputAllContent();
+            database.NewEntry(new List<string> { "niorg2606", "password123" });
+            database.NewEntry(new List<string> { "SixBeeps", "anotherPassword321" });
+            OutputAllContent();
         }
-      }
-      database.SetLabel(0,"Username");
-      database.SetLabel(1,"Password");
-      database.NewEntry(new List<string> {"niorg2606", "password"});
-      foreach (List<string> secondaryList in database.GetAllContent()) {
-        foreach (string s in secondaryList) {
-          Console.WriteLine(s);
+
+        static void OutputAllContent()
+        {
+            foreach(List<string> secondaryList in database.GetAllContent())
+            {
+                foreach(string s in secondaryList)
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
-      }
     }
-  }
 }
