9298275d37603c4d45f77cc4286733b190789103
Brandon Lee <brandononlyu@gmail.com>
2017-09-30 12:33:34 -0400

    Fixed Datab.cs
    Arrays are a bad so i fixed that

--- Datab.cs
+++ Datab.cs
@@ -1,39 +1,69 @@
-namespace Datab { //T h i s n a m e s p a c e c a n n o t b e r u n i n c o m p i l e r m o d e
-  public static class Database {
-    private static List<string> Labels;
-    private static List<List<string>> Contents;
-    public static void Init() {
-      //Initialize the contents table
-      Contents = new List<List<string>>();
-    }
-    public static void SetLabel(int at, string name) {
-      Labels[at] = name;
-    }
-    public static string GetLabel(int at) {
-      return Labels[at];
-    }
-    public static int GetLabelID(string name) {
-      int temp = 0;
-      foreach (string s in Labels) {
-        if (s == name) {
-          return temp;
-        } else {
-          temp++;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Datab
+{ //T h i s n a m e s p a c e c a n n o t b e r u n i n c o m p i l e r m o d e
+    public class Database
+    {
+        private List<string> Labels;
+        private List<List<string>> Contents;
+        public void Init()
+        {
+            //Initialize the contents table
+            Contents = new List<List<string>> { };
+            Labels = new List<string> { };
+            
         }
-      }
-      return 0;
+        public void SetLabel(int at, string name)
+        {
+            if (at > Labels.Count)
+            {
+                for (int i = Labels.Count; i < at; i++)
+                {
+                    Labels.Add("");
+                }
+                Labels.Add(name);
+            }
+            else
+            {
+                Labels.Insert(at, name);
+            }
+        }
+        public string GetLabel(int at)
+        {
+            return Labels[at];
+        }
+        public int GetLabelID(string name)
+        {
+            int temp = 0;
+            foreach (string s in Labels)
+            {
+                if (s == name)
+                {
+                    return temp;
+                }
+                else
+                {
+                    temp++;
+                }
+            }
+            return 0;
+        }
+        public string GetContent(int labelID, int entry)
+        {
+            return Contents[labelID][entry];
+        }
+        public string GetContent(string label, int entry)
+        {
+            return Contents[GetLabelID(label)][entry];
+        }
+        public void NewEntry(List<string> content)
+        {
+            Contents.Add(content);
+        }
+        public List<List<string>> GetAllContent()
+        {
+            return Contents;
+        }
     }
-    public static string GetContent(int labelID, int entry) {
-      return Contents[labelID][entry];
-    }
-    public static string GetContent(string label, int entry) {
-      return Contents[GetLabelID(label)][entry];
-    }
-    public static void NewEntry(List<string> content) {
-      Contents.Insert(Contents.Count-1, content);
-    }
-    public static List<List<string>> GetAllContent() {
-      return Contents;
-    }
-  }
 }
